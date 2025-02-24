013e2d60d54fd913849b930a3d3e93bcad28dbee
Alaksion <lbeurmann.dev@gmail.com>
2025-02-24 01:09:20 -0300

    feat: init compose web

---
+++ composeApp/src/wasmJsMain/kotlin/io/github/alaksiondev/invoicerweb/Greeting.kt
@@ -0,0 +1,9 @@
+package io.github.alaksiondev.invoicerweb
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
--- lib/features/auth/domain/repository/auth_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:invoicer_web/features/auth/domain/model/sign_in_request_model.dart';
-import 'package:invoicer_web/features/auth/domain/model/sign_in_response_model.dart';
-
-abstract class AuthRepository {
-  Future<SignInResponseModel> signIn(SignInRequestModel request);
-}
