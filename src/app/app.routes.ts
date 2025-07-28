ccb17170b8a90b60ed8c4943f11d87d71ee91bc8
Alaksion <lbeurmann.dev@gmail.com>
2025-07-28 20:47:39 -0300

    feat: partial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from "@angular/router";
+import { AuthRoutes } from "../presentation/screens/auth/auth.routes";
+
+export const routes: Routes = AuthRoutes;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from "@angular/platform-browser";
-import { appConfig } from "./config/app.config";
+import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 import { LoginComponent } from "./presentation/screens/auth/login/login.component";
 
