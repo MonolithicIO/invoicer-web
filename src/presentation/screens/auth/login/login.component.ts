7c2768ed9e7b0043871abe5060c4b4bd0d1fca3e
Alaksion <lbeurmann.dev@gmail.com>
2025-07-28 22:40:07 -0300

    feat: force lint error

--- src/presentation/screens/auth/login/login.component.ts
+++ src/presentation/screens/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: "app-login-screen",
+  selector: "login-screen",
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
   imports: [RouterLink],
