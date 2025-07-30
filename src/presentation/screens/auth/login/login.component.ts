bb7f7dba65dbc2706b0be7d4f465dab66c45a67c
Alaksion <lbeurmann.dev@gmail.com>
2025-07-30 00:52:21 -0300

    feat: implement primitive login ui

--- src/presentation/screens/auth/login/login.component.ts
+++ src/presentation/screens/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal, computed } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -7,4 +7,26 @@ import { RouterLink } from "@angular/router";
   styleUrl: "./login.component.css",
   imports: [RouterLink],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = signal("");
+  password = signal("");
+  isButtonEnabled = computed(() => this.email() && this.password());
+
+  onChangeEmail(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.email.set(input.value);
+  }
+
+  onChangePassword(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.password.set(input.value);
+  }
+
+  onLogin() {
+    alert(`Email: ${this.email()}, Password: ${this.password()}`);
+  }
+
+  onLoginWithGoogle() {
+    alert("Login with Google is not implemented yet.");
+  }
+}
