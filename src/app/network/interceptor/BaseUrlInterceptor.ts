53dcb3a987f9c6a5f0730834412e438d71794d50
Alaksion <lbeurmann.dev@gmail.com>
2025-08-10 18:00:04 -0300

    feat: use env to provide base url

--- src/app/network/interceptor/BaseUrlInterceptor.ts
+++ src/app/network/interceptor/BaseUrlInterceptor.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment.development";
 
 export function BaseUrlInterceptor(
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<unknown>> {
   const apiRequest = req.clone({
-    url: `http://localhost:8080${req.url}`,
+    url: `${environment.apiUrl}${req.url}`,
   });
 
   return next(apiRequest);
