01d419f70751962e0107955cc89aa4145bd66a97
Alaksion <lbeurmann.dev@gmail.com>
2025-08-17 22:04:44 -0300

    feat: implement guard

--- src/app/presentation/navigation/guards/CompanySelectedGuard.ts
+++ src/app/presentation/navigation/guards/CompanySelectedGuard.ts
@@ -1,3 +1,16 @@
-import { CanActivateFn } from "@angular/router";
+import { CanActivateFn, Router } from "@angular/router";
+import { CompanyRepository } from "../../../domain/company/repository/CompanyRepository";
+import { inject } from "@angular/core";
 
-export const CompanySelectedGuard: CanActivateFn = () => {};
+export const CompanySelectedGuard: CanActivateFn = () => {
+  const repository = inject(CompanyRepository);
+  const router = inject(Router);
+  const isCompanySelected = repository.getSelectedCompany() !== null;
+
+  if (isCompanySelected) {
+    return true;
+  } else {
+    router.navigate(["/select-company"]);
+    return false;
+  }
+};
