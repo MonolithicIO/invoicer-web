ee23626689776a47b8183daeddbc62f1c4939bd7
Alaksion <lbeurmann.dev@gmail.com>
2025-08-20 21:54:03 -0300

    feat: basic styling

--- src/app/presentation/screens/signup/signup.component.ts
+++ src/app/presentation/screens/signup/signup.component.ts
@@ -1,10 +1,49 @@
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "app-signup-screen",
   templateUrl: "./signup.component.html",
   styleUrls: ["./signup.component.css"],
-  imports: [RouterLink],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+  ],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  email = signal("");
+  confirmEmail = signal("");
+  password = signal("");
+  showPassword = signal(false);
+
+  onChangeEmail(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    this.email.set(input.value);
+  }
+
+  onChangeConfirmEmail(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.confirmEmail.set(input.value);
+  }
+
+  onChangePassword(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.password.set(input.value);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword.set(!this.showPassword());
+  }
+
+  onSubmit() {
+    alert("hello world");
+  }
+}
