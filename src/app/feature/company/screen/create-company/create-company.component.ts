3637f569dc945f507621e3e4a5c20e609d1a2bfe
Alaksion <lbeurmann.dev@gmail.com>
2025-09-01 20:52:05 -0300

    feat: implement whole form validation + button

--- src/app/feature/company/screen/create-company/create-company.component.ts
+++ src/app/feature/company/screen/create-company/create-company.component.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { RequiredFieldFormField } from "./form-fields/create-company-form-fields";
 import { FormControlErrorWatcherGroup } from "../../../../../core/util/form-control-error-watcher";
+import { MatDividerModule } from "@angular/material/divider";
 
 @Component({
   selector: "app-create-company",
@@ -25,6 +26,7 @@ import { FormControlErrorWatcherGroup } from "../../../../../core/util/form-cont
     ReactiveFormsModule,
     MatIconModule,
     MatCardModule,
+    MatDividerModule,
   ],
   templateUrl: "./create-company.component.html",
   styleUrl: "./create-company.component.css",
@@ -52,12 +54,25 @@ export class CreateCompanyComponent implements OnInit {
 
   readonly nameErrorText = signal<string | null>("");
   readonly documentErrorText = signal<string | null>("");
+  readonly name = signal<string | null>(null);
+  readonly document = signal<string | null>(null);
+  readonly addressLine1 = signal<string | null>(null);
+  readonly addressLine2 = signal<string | null>(null);
+  readonly city = signal<string | null>(null);
+  readonly state = signal<string | null>(null);
+  readonly postalCode = signal<string | null>(null);
+  readonly primaryIban = signal<string | null>(null);
+  readonly primarySwift = signal<string | null>(null);
+  readonly primaryBankName = signal<string | null>(null);
+  readonly primaryBankAddress = signal<string | null>(null);
 
   ngOnInit(): void {
-    this.identityFormFields.startWatchFormChanges();
+    this.identityWatcherGroup.startWatchFormChanges();
+    this.addressWatcherGroup.startWatchFormChanges();
+    this.primaryAccountWatcher.startWatchFormChanges();
   }
 
-  private identityFormFields = new FormControlErrorWatcherGroup({
+  private identityWatcherGroup = new FormControlErrorWatcherGroup({
     name: new RequiredFieldFormField(
       "Company name",
       this.getFormControllerByName("name"),
@@ -70,7 +85,62 @@ export class CreateCompanyComponent implements OnInit {
     ),
   });
 
+  private addressWatcherGroup = new FormControlErrorWatcherGroup({
+    addressLine1: new RequiredFieldFormField(
+      "Address Line 1",
+      this.getFormControllerByName("addressLine1"),
+      this.addressLine1
+    ),
+    addressLine2: new RequiredFieldFormField(
+      "Address Line 2",
+      this.getFormControllerByName("addressLine2"),
+      this.addressLine2
+    ),
+    city: new RequiredFieldFormField(
+      "City",
+      this.getFormControllerByName("city"),
+      this.city
+    ),
+    state: new RequiredFieldFormField(
+      "State",
+      this.getFormControllerByName("state"),
+      this.state
+    ),
+    postalCode: new RequiredFieldFormField(
+      "Postal Code",
+      this.getFormControllerByName("postalCode"),
+      this.postalCode
+    ),
+  });
+
+  private primaryAccountWatcher = new FormControlErrorWatcherGroup({
+    primaryIban: new RequiredFieldFormField(
+      "IBAN",
+      this.getFormControllerByName("primaryIban"),
+      this.primaryIban
+    ),
+    primarySwift: new RequiredFieldFormField(
+      "SWIFT",
+      this.getFormControllerByName("primarySwift"),
+      this.primarySwift
+    ),
+    primaryBankName: new RequiredFieldFormField(
+      "Bank Name",
+      this.getFormControllerByName("primaryBankName"),
+      this.primaryBankName
+    ),
+    primaryBankAddress: new RequiredFieldFormField(
+      "Bank Address",
+      this.getFormControllerByName("primaryBankAddress"),
+      this.primaryBankAddress
+    ),
+  });
+
   private getFormControllerByName(name: string): FormControl {
     return this.createCompanyForm.get(name) as FormControl;
+  }
+
+  onSubmit() {
+    alert("Not implemented yet");
   }
 }
