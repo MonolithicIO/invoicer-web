0da258cd7cfd003d24b46f2f2e9ebb4dedce4bfd
Alaksion <lbeurmann.dev@gmail.com>
2025-09-02 19:10:20 -0300

    feat: refresh session on logout

--- src/app/feature/auth/service/logout.service.ts
+++ src/app/feature/auth/service/logout.service.ts
@@ -3,15 +3,18 @@ import { Observable, of } from "rxjs";
 
 import { AuthTokenRepository } from "../repository/auth-token.repository";
 import { CompanyRepository } from "../../company/repository/company.repository";
+import { SessionRefresherService } from "../../session/service/session-refresher.service";
 
 @Injectable({ providedIn: "root" })
 export class LogoutService {
-  private authTokenRepository = inject(AuthTokenRepository);
-  private companyRepository = inject(CompanyRepository);
+  private readonly authTokenRepository = inject(AuthTokenRepository);
+  private readonly companyRepository = inject(CompanyRepository);
+  private readonly sessionRefresher = inject(SessionRefresherService);
 
   logout(): Observable<void> {
     this.authTokenRepository.clearTokens();
     this.companyRepository.clearSelectedCompany();
+    this.sessionRefresher.refreshSession();
     return of(void 0);
   }
 }
